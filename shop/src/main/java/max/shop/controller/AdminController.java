8d6a26ea6856a24be395af26bbfddf8c5948c026
Max96312 <k0mp4ny16@gmail.com>
2024-05-13 21:45:02 +0900

    Feat: admin main view template 작성 완료 1:1 문의는 추후에 작성 예정

--- shop/src/main/java/max/shop/controller/AdminController.java
+++ shop/src/main/java/max/shop/controller/AdminController.java
@@ -1,32 +1,17 @@
 package max.shop.controller;
 
 import lombok.RequiredArgsConstructor;
-//import max.shop.dto.request.CreateProductRequest;
-import max.shop.response.ResponseService;
-import max.shop.service.ItemService;
+import max.shop.service.item.ItemServiceImpl;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * /item/{id} GET : 상품 조회
- * /item/{id} PUT : 상품 수정
- * /item POST : 상품 등록
- * /items GET : 상품 목록 조회
- */
-
-@RestController
+@Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
 
-    private final ItemService productService;
-    private final ResponseService responseService;
-
-    @GetMapping("/item/{id}")
-    public void product(@PathVariable String id){
+    @GetMapping("/home")
+    public String home(){
+        return "home";
     }
-
-//    @PostMapping
-//    public SingleResult<ProductResponse> postNewProduct(@RequestBody CreateProductRequest product){
-//        productService.createProduct(product);
-//    }
 }
