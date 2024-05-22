a37bd6dee705e31d7ad9c2b00e87c16a3285c6c2
Max96312 <k0mp4ny16@gmail.com>
2024-05-23 03:09:59 +0900

    Feat: app service용 쿼리 추가

--- shop/src/main/java/max/shop/repository/ItemRepository.java
+++ shop/src/main/java/max/shop/repository/ItemRepository.java
@@ -1,9 +1,16 @@
 package max.shop.repository;
 
 import max.shop.domain.Item;
+import max.shop.domain.type.ItemType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("select i from Item i where i.type = :request and i.itemStatus not in ('HIDE', 'REMOVE')")
+    Page<Item> findItemPage(ItemType request, Pageable pageable);
 }
