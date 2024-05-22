b2d8a87591df6a939b47717213ce7e67d75a276d
Max96312 <k0mp4ny16@gmail.com>
2024-05-22 17:24:23 +0900

    Refactor: categories & cart 엔티티 제거 ItemType으로 관리

--- shop/src/main/java/max/shop/repository/CategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package max.shop.repository;
-
-import max.shop.domain.Categories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriesRepository extends JpaRepository<Categories, Long> {
-}
