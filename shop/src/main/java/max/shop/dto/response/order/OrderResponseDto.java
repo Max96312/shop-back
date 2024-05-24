b17f7050cea396d8e9e051bed1ab89fc2ee73b47
Max96312 <k0mp4ny16@gmail.com>
2024-05-24 18:44:10 +0900

    Feat: Reponse

---
+++ shop/src/main/java/max/shop/dto/response/order/OrderResponseDto.java
@@ -0,0 +1,12 @@
+package max.shop.dto.response.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponseDto {
+    private Long id;
+}
